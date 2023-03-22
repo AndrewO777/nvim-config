15590953dde1f1a2331e9f1b7d00f504a66ede87
AndrewO <andrewodom777@gmail.com>
2023-03-22 17:00:23 -0400

    initial commit

---
+++ lua/remap.lua
@@ -0,0 +1,4 @@
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("i", "<C-c>", "<Esc>")
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
