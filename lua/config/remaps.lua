f143cb0da3b4914840745c8cf5ab51083d262c7d
andrew <andrew@andrewodom.net>
2026-02-18 20:43:29 -0500

    added diagnostic remap

--- lua/config/remaps.lua
+++ lua/config/remaps.lua
@@ -2,3 +2,5 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("i", "<C-c>", "<Esc>")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>e", function() vim.diagnostic.open_float() end)
+
